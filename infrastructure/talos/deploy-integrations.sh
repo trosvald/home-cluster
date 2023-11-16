32c9ef186a2bb7c5abed7e924ad9182ca677792a
Eko Purwanto <eko.pur25@gmail.com>
2023-11-16 15:34:19 +0700

    cleanup talos

--- infrastructure/talos/deploy-integrations.sh
+++ infrastructure/talos/deploy-integrations.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# shellcheck disable=2312
+pushd integrations >/dev/null 2>&1 || exit 1
 
-pushd integrations >/dev/null 2>&1
-
-if test -d cni/charts; then
-  rm -rf cni/charts
-fi
-envsubst < ../../../kubernetes/apps/kube-system/cilium/app/values.yaml  > cni/values.yaml
+rm -rf cni/charts
+envsubst < ../../../kubernetes/apps/kube-system/cilium/app/values.yaml > cni/values.yaml
 kustomize build --enable-helm cni | kubectl apply -f -
-rm -r cni/values.yaml cni/charts
+rm cni/values.yaml
+rm -rf cni/charts
 
-if test -d csr-approver/charts; then
-  rm -rf csr-approver/charts
+rm -rf kubelet-csr-approver/charts
+envsubst < ../../../kubernetes/apps/system-controllers/kubelet-csr-approver/app/values.yaml > kubelet-csr-approver/values.yaml
+if ! kubectl get ns system-controllers >/dev/null 2>&1; then
+  kubectl create ns system-controllers
 fi
-envsubst < ../../../kubernetes/apps/system-controllers/kubelet-csr-approver/app/values.yaml > csr-approver/values.yaml
-kustomize build --enable-helm csr-approver | kubectl apply -f -
-rm -r csr-approver/values.yaml csr-approver/charts
-popd >/dev/null 2>&1
+kustomize build --enable-helm kubelet-csr-approver | kubectl apply -f -
+rm kubelet-csr-approver/values.yaml
+rm -rf kubelet-csr-approver/charts
